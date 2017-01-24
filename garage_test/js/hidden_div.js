8cf371bbaa80f82a3dead25f2afb1eea8f9a8bbd
fr0st55 <dimka5508@gmail.com>
2017-01-24 21:19:55 +0200

    added slider alpha-version

--- garage_test/js/hidden_div.js
+++ garage_test/js/hidden_div.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
-    $(".hider").click(function(){
+    $(".hider_but").click(function(){
         if ($(".hidden").is(":hidden")) {
         	$(".hidden").fadeIn("slow");
+        	$(".hider").show();
+        	$(".shower").hide();
         } else {
         	  $(".hidden").fadeOut("fast");
+        	  $(".hider").hide();
+        	  $(".shower").show();
         }
         return false;
     });
